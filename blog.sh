e109774861162dea8e83c6152ec2e66b7c2e0941
laughingbiscuit <sean@laughingbiscuit.com>
2023-09-14 15:35:13 +0000

    plantuml examples

--- blog.sh
+++ blog.sh
@@ -14,6 +14,14 @@
 # My notepad as a shell script
 # 
 ###
+# 1 Introduction
+###
+#
+# - Sean Davis
+# - API Geek
+# - Biker
+#
+###
 # 2 Dev Environment Setup
 ###
 #
@@ -25,7 +33,44 @@
 #
 function install_dev_env {
   apk update
-  apk add git tmux curl busybox-extras pandoc gettext
+  apk add git tmux curl busybox-extras pandoc gettext openjdk17
+  curl -sSL "https://github.com/plantuml/plantuml/releases/download/v1.2023.11/plantuml-1.2023.11.jar" -o /opt/plantuml.jar
+}
+function install_dev_env_desktop {
+  apk update
+  apk add xfce-terminal dwm chromium
+}
+###
+# 3.1. Dev Env Demo - PlantUML
+###
+function demo_plantuml_seq {
+  cat << EOF | java -jar /opt/plantuml.jar -ttxt
+@startuml
+a->b: GET /
+@enduml
+EOF
+}
+# <img src="demo_plantuml_component.png">
+function demo_plantuml_component {
+  cat << EOF | java -jar /opt/plantuml.jar > demo_plantuml_component.png
+@startuml
+cloud GCP {
+  rectangle uSvc as ms
+  database Database as db
+  ms->db
+}
+@enduml
+EOF
+}
+# <img src="demo_plantuml_flow.png">
+function demo_plantuml_flow {
+  cat << EOF | java -jar /opt/plantuml.jar > demo_plantuml_flow.png
+@startuml
+:step one;
+:step two;
+@enduml
+}
+EOF
 }
 
 "$@"
