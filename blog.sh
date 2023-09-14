33447ca6fab35987b1ce1dde60363a93859dc0c9
laughingbiscuit <sean@laughingbiscuit.com>
2023-09-14 16:00:41 +0000

    space out

--- blog.sh
+++ blog.sh
@@ -10,50 +10,61 @@
 #                ||----w |
 #                ||     ||
 ###
-#
+
 # My notepad as a shell script
-# 
+
 ###
 # 1 Introduction
 ###
-#
+
 # - Sean Davis
 # - API Geek
 # - Biker
-#
+
 ###
 # 2 Dev Environment Setup
 ###
-#
+
 # Since Alpine Linux 3.16, DNS over TCP issues have been resolved. Its
 # time to give it another try!
-#
+
 # To stay simple and portable, my minimal environment will use a few core
 # tools in addition to busybox.
-#
+
 function install_dev_env {
   apk update
   apk add git tmux curl busybox-extras pandoc gettext openjdk17 graphviz
   curl -sSL "https://github.com/plantuml/plantuml/releases/download/v1.2023.11/plantuml-1.2023.11.jar" -o /opt/plantuml.jar
   java -jar /opt/plantuml.jar -testdot
 }
+
 function install_dev_env_desktop {
   apk update
   apk add dwm chromium xterm
 }
+
 ###
 # 3.1. Dev Env Demo - PlantUML
 ###
+
 function demo_plantuml_seq {
   cat << EOF | java -jar /opt/plantuml.jar -p -ttxt
+
+
 @startuml
 a->b: GET /
 @enduml
+
+
 EOF
 }
+
 # <img src="demo_plantuml_component.png">
+
 function demo_plantuml_component {
   cat << EOF | java -jar /opt/plantuml.jar -p > demo_plantuml_component.png
+
+
 @startuml
 cloud GCP {
   rectangle uSvc as ms
@@ -61,17 +72,25 @@ cloud GCP {
   ms->db
 }
 @enduml
+
+
 EOF
 }
+
 # <img src="demo_plantuml_flow.png">
+
 function demo_plantuml_flow {
   cat << EOF | java -jar /opt/plantuml.jar -p > demo_plantuml_flow.png
+
+
 @startuml
 :step one;
 :step two;
 @enduml
-}
+
+
 EOF
 }
 
+# fin
 "$@"
