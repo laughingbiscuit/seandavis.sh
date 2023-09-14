1bbb4e0e974f7d76aa1240eab8da7290703961f2
laughingbiscuit <sean@laughingbiscuit.com>
2023-09-14 15:49:51 +0000

    increase width

--- gen.sh
+++ gen.sh
@@ -9,4 +9,4 @@ cp blog.sh target/
 cd target/
 
 cat blog.sh | grep -e "^function" | sed 's/function //' | sed 's/{//' | xargs -I{}  sh -c "sh blog.sh {}"
-echo -e "\`\`\`\n\$SCRIPT\n\`\`\`" | pandoc -A after.htmlsnip --metadata="title=seandavis.sh" -s | SCRIPT="$(cat blog.sh)" envsubst > index.html
+echo -e "\`\`\`\n\$SCRIPT\n\`\`\`" | pandoc -H head.htmlsnip -A after.htmlsnip --metadata="title=seandavis.sh" -s | SCRIPT="$(cat blog.sh)" envsubst > index.html
