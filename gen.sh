d70adf2f45554895dbfda264ef9ab118840f2194
laughingbiscuit <sean@laughingbiscuit.com>
2023-09-19 08:26:26 +0000

    final renames

--- gen.sh
+++ gen.sh
@@ -5,9 +5,9 @@ sh -e notes.sh install_dev_env
 rm -rf target/
 mkdir target/
 cp static/* target/
-cp blog.sh target/
+cp notes.sh target/
 cd target/
 
-cat notes.sh | grep -e "^function" | sed 's/function //' | sed 's/{//' | xargs -I{}  sh -c "sh blog.sh {}"
-pandoc -H head.htmlsnip -A after.htmlsnip --metadata="title=seandavis.sh" -s index.md | SCRIPT="$(cat blog.sh)" envsubst > index.html
-mv blog.sh raw
+cat notes.sh | grep -e "^function" | sed 's/function //' | sed 's/{//' | xargs -I{}  sh -c "sh notes.sh {}"
+pandoc -H head.htmlsnip -A after.htmlsnip --metadata="title=seandavis.sh" -s index.md | SCRIPT="$(cat notes.sh)" envsubst > index.html
+mv notes.sh raw
