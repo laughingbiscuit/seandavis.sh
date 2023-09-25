3ec6951424f29010d1a72b53e466b332194c4a7b
laughingbiscuit <sean@laughingbiscuit.com>
2023-09-25 17:58:27 +0000

    debug cast

--- gen.sh
+++ gen.sh
@@ -11,4 +11,5 @@ cd target/
 cat notes.sh | grep -e "^function" | sed 's/function //' | sed 's/{//' | xargs -I{}  sh -e -c "sh -e notes.sh {}"
 pandoc -H head.htmlsnip -A after.htmlsnip --metadata="title=seandavis.sh" -s index.md | SCRIPT="$(cat notes.sh)" envsubst > index.html
 mv notes.sh raw
+cat out.cast
 sed -i '1s/.*/\{"version": 2, "width": 66, "height": 37, "timestamp": 1695663471, "env": \{"SHELL": "/bin/ash", "TERM": "xterm-256color"}}/' out.cast
