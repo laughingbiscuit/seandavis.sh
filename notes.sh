767c432cc858562eec5d67dbb9d5cfdce51bbce5
laughingbiscuit <sean@laughingbiscuit.com>
2023-09-25 16:02:01 +0000

    add asciiplayer

--- notes.sh
+++ notes.sh
@@ -41,17 +41,12 @@ http://dl-cdn.alpinelinux.org/alpine/edge/testing
 EOF
 
   apk update
-  apk add git tmux curl busybox-extras pandoc gettext openjdk17 graphviz kubectl docker expect asciinema font-jetbrains-mono nodejs
+  apk add git tmux curl busybox-extras pandoc gettext openjdk17 graphviz kubectl docker expect asciinema
 
 
   curl -sSL \
     "https://github.com/plantuml/plantuml/releases/download/v1.2023.11/plantuml-1.2023.11.jar" \
     -o /opt/plantuml.jar
-#  curl -sSL \
-#    "https://github.com/asciinema/agg/releases/download/v1.4.2/agg-x86_64-unknown-linux-musl" \
-#    -o /usr/bin/agg
-#  chmod +x /usr/bin/agg
-  npm i -g asciicast2gif
   java -jar /opt/plantuml.jar -testdot
   curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | sh
 
@@ -177,13 +172,9 @@ send -h "exit\r"
 
 EOF
 
-  cat out.cast
-  uname -a
-#  RUST_BACKTRACE=full agg out.cast out.gif
-  asciicast2gif out.cast out.gif
 }
 # Output:
-# <img src="out.gif">
+# <div id="demo_expect_asciinema_player"></div><script>AsciinemaPlayer.create('/demo.cast', document.getElementById('demo_expect_asciinema_player'));</script>
 
 # Thank you!
 "$@"
