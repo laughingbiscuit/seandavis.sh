12833fe4fe603dc1d83fe59cb4df579ad811eca6
laughingbiscuit <sean@laughingbiscuit.com>
2023-09-25 18:07:31 +0000

    use extended regex

--- gen.sh
+++ gen.sh
@@ -12,4 +12,4 @@ cat notes.sh | grep -e "^function" | sed 's/function //' | sed 's/{//' | xargs -
 pandoc -H head.htmlsnip -A after.htmlsnip --metadata="title=seandavis.sh" -s index.md | SCRIPT="$(cat notes.sh)" envsubst > index.html
 mv notes.sh raw
 cat out.cast
-sed -i '1s/.*/\{"version": 2, "width": 66, "height": 37, "timestamp": 1695663471, "env": \{"SHELL": "/bin/ash", "TERM": "xterm-256color"}}/' out.cast
+sed -ei '1s/.*/\{"version": 2, "width": 66, "height": 37, "timestamp": 1695663471, "env": \{"SHELL": "/bin/ash", "TERM": "xterm-256color"}}/' out.cast
