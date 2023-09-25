decc19b45332e5ede0dab642037eb0451945e16b
laughingbiscuit <sean@laughingbiscuit.com>
2023-09-25 17:43:51 +0000

    does the first line make a difference?

--- gen.sh
+++ gen.sh
@@ -11,3 +11,4 @@ cd target/
 cat notes.sh | grep -e "^function" | sed 's/function //' | sed 's/{//' | xargs -I{}  sh -e -c "sh -e notes.sh {}"
 pandoc -H head.htmlsnip -A after.htmlsnip --metadata="title=seandavis.sh" -s index.md | SCRIPT="$(cat notes.sh)" envsubst > index.html
 mv notes.sh raw
+sed -i '1s/.*/{"version": 2, "width": 66, "height": 37, "timestamp": 1695663471, "env": {"SHELL": "/bin/ash", "TERM": "xterm-256color"}}/' out.cast
