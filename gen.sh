6f6c5b7a432c5f92173a0429217b71fe65406f7d
laughingbiscuit <sean@laughingbiscuit.com>
2023-09-26 13:38:34 +0000

    fix json

--- gen.sh
+++ gen.sh
@@ -12,5 +12,5 @@ cat notes.sh | grep -e "^function" | sed 's/function //' | sed 's/{//' | xargs -
 pandoc -H head.htmlsnip -A after.htmlsnip --metadata="title=seandavis.sh" -s index.md | SCRIPT="$(cat notes.sh)" envsubst > index.html
 mv notes.sh raw
 cat out.cast | tail -n +2 > out.snip
-echo '{"version": 2, "width": 66, "height": 37, "timestamp": 1695663471, "env": \{"SHELL": "/bin/ash", "TERM": "xterm-256color"}}' > out.cast
+echo '{"version": 2, "width": 66, "height": 37, "timestamp": 1695663471, "env": {"SHELL": "/bin/ash", "TERM": "xterm-256color"}}' > out.cast
 cat out.snip >> out.cast
